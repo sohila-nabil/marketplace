462fcfe4c5757fb4c4f98dd6be00a61299afbc1a
unknown <nsohila03@gmail.com>
2025-02-17 15:27:01 +0200

    updated

--- server/index.js
+++ server/index.js
@@ -38,6 +38,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Interal Server Error";
